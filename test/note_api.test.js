4f20dea52dde10fa37c96f3ca8e015bbf52a143f
askar-dev <askar200018@mail.ru>
2022-06-29 18:22:18 +0600

    4.b - Error handling and async/await

--- test/note_api.test.js
+++ test/note_api.test.js
@@ -27,7 +27,7 @@ test('there are two notes', async () => {
   expect(response.body).toHaveLength(helper.initialNotes.length)
 })
 
-test('a specifi note is within the returned notes', async () => {
+test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
 
   const contents = response.body.map((r) => r.content)
@@ -63,6 +63,35 @@ test('note without content not added', async () => {
   const notesAtEnd = await helper.notesInDb()
 
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
+})
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+
+  expect(resultNote.body).toEqual(processedNoteToView)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToRemove = notesAtStart[0]
+
+  await api.delete(`/api/notes/${noteToRemove.id}`).expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  expect(notesAtEnd).toHaveLength(notesAtStart.length - 1)
+
+  const contents = notesAtEnd.map((r) => r.content)
+
+  expect(contents).not.toContain(noteToRemove.content)
 })
 
 afterAll(() => {
