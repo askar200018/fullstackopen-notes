6a547306379fdcdb38fb3869581951a981440c18
askar-dev <askar200018@mail.ru>
2022-06-29 18:38:15 +0600

    4.b - Optimizing the beforeEach function

--- controllers/notes.js
+++ controllers/notes.js
@@ -6,20 +6,16 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
-notesRouter.get('/:id', async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  } catch (exception) {
-    next(exception)
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
   }
 })
 
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -28,24 +24,16 @@ notesRouter.post('/', async (request, response, next) => {
     date: new Date(),
   })
 
-  try {
-    const savedNote = await note.save()
-    response.status(201).json(savedNote)
-  } catch (exception) {
-    next(exception)
-  }
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Note.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (exception) {
-    next(exception)
-  }
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', (request, response) => {
   const body = request.body
 
   const note = {
@@ -53,12 +41,8 @@ notesRouter.put('/:id', (request, response, next) => {
     important: body.important,
   }
 
-  try {
-    const updatedNote = Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    response.json(updatedNote)
-  } catch (exception) {
-    next(exception)
-  }
+  const updatedNote = Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  response.json(updatedNote)
 })
 
 module.exports = notesRouter
